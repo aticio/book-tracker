26318189049eaf91832d053d9009aad356ce4c70
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2022-08-05 08:40:52 +0000

    on close bug fix

--- book-tracker.py
+++ book-tracker.py
@@ -31,7 +31,7 @@ def on_error(w_s, error):
     logging.error(error)
 
 
-def on_close(w_s):
+def on_close(w_s, close_status_code, close_msg):
     logging.info("closing websocket connection, initiating again...")
     init_stream()
 
@@ -47,7 +47,7 @@ def on_message(w_s, message):
         buy_side = float(book_ticker_data['a']) * float(book_ticker_data['A'])
         delta = buy_side - sell_side
         data = f"book_ticker,symbol={book_ticker_data['s']} delta={delta}"   
-        with InfluxDBClient(url="http://localhost:8086", token=token, org=org) as client:
+        with InfluxDBClient(url="http://ec2-52-15-110-102.us-east-2.compute.amazonaws.com:8086", token=token, org=org) as client:
             write_api = client.write_api(write_options=SYNCHRONOUS)
             write_api.write(bucket, org, data)
             client.close()
